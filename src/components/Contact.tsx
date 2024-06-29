1013c64b2cc9623d9901c3d0fb1536b81cb10de2
JimmyClak <sebastian@clak.app>
2024-06-29 14:50:02 -0300

    some fixes and contact component updated

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,58 +1,69 @@
-import React from 'react';
+import type React from 'react';
+// eslint-disable-next-line no-duplicate-imports
+import { useState } from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
-const Contact: React.FC = () =>  {
+const Contact: React.FC = () => {
+  const [copied, setCopied] = useState(false);
+
   return (
     <section id="contact" className="my-8 bg-white shadow-md rounded-lg p-6">
       <div className="max-w-4xl mx-auto">
         <h2 className="text-2xl font-bold text-gray-800 mb-4">Contact</h2>
-        <p className="text-lg text-gray-600 mb-4">
-            If you want to contact me..
-        </p>
-        <div className="flex items-center space-x-4">
-            <a
-                href="https://github.com/tu_usuario"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-2xl text-gray-800 hover:text-gray-600"
+        <div className="text-lg text-gray-600 mb-4">
+          <p>You can reach me at:</p>
+          <div className="flex items-center space-x-2">
+            <span className="font-semibold">martiniseba78@gmail.com</span>
+            <CopyToClipboard
+              text="martiniseba78@gmail.com"
+              onCopy={() => {setCopied(true)}}
             >
-            <img
-            src="/github-icon.png"
-            alt="github"
-            className="w-10 h-10 object-cover"
-            />
-          </a>
+              <button className="text-white p-1 rounded-md hover:bg-slate-100 transition duration-300 flex items-center">
+                <img
+                  src={copied ? '/green-check-icon.png' : '/clip-icon.png'}
+                  alt={copied ? 'Copied' : 'Copy'}
+                  className="w-5 h-5"
+                />
+              </button>
+            </CopyToClipboard>
+          </div>
+        </div>
+        <p className="text-lg text-gray-600 mb-4">My social medias:</p>
+        <div className="flex items-center space-x-4">
           <a
-            href="mailto:tuemail@gmail.com"
-            className="text-2xl text-gray-800 hover:text-gray-600"
-          >
-            <img
-            src="/gmail-icon.png"
-            alt="gmail"
-            className="w-14 h-14 object-cover"
-            />
-          </a>
-          <a
-            href="https://twitter.com/tu_usuario"
+            href="https://github.com/Jimboy78"
             target="_blank"
             rel="noopener noreferrer"
-            className="text-2xl text-gray-800 hover:text-gray-600"
+            className="text-2xl text-gray-800 hover:text-gray-600 transition duration-300"
           >
             <img
-            src="/x-icon.png"
-            alt="x"
-            className="w-14 h-14 object-cover"
+              src="/github-icon.png"
+              alt="github"
+              className="w-10 h-10 object-cover"
             />
           </a>
           <a
-            href="https://www.linkedin.com/in/tu_usuario"
+            href="https://x.com/Jimboy_002"
             target="_blank"
             rel="noopener noreferrer"
-            className="text-2xl text-gray-800 hover:text-gray-600"
+            className="text-2xl text-gray-800 hover:text-gray-600 transition duration-300"
           >
             <img
-            src="/likedIn-icon.png"
-            alt="likedIn"
-            className="w-10 h-10 object-cover"
+              src="/x-icon.png"
+              alt="x"
+              className="w-14 h-14 object-cover"
+            />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/sebastian-martini/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-2xl text-gray-800 hover:text-gray-600 transition duration-300"
+          >
+            <img
+              src="/linkedin-icon.png"
+              alt="linkedin"
+              className="w-10 h-10 object-cover"
             />
           </a>
         </div>
